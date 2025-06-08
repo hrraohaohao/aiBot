215b8106f8eec4cfedd60274db3d198c5e36632a
饶浩浩 <470420709@qq.com>
2025-06-08 15:39:36 +0800

    android 存储兼容问题

--- android/app/src/main/kotlin/com/example/ai_bot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ai_bot/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.ai_bot
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.multidex.MultiDex
+import android.content.Context
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
